ce15474f537912891f8cef8c82007548e7598532
NagaSaiKiranG <doitkiran@outlook.com>
2022-05-08 09:26:38 +0530

    constants updated

--- src/package/package.constants.js
+++ src/package/package.constants.js
@@ -1,5 +1,5 @@
-export const git_base_url = process.eventNames.GIT_BASE_URL || 'https://api.github.com/';
-export const search_repo_path = process.eventNames.GIT_SEARCH_REPOSITORY_PATH || "search/repositories";
+export const git_base_url = process.env.GIT_BASE_URL || 'https://api.github.com/';
+export const search_repo_path = process.env.GIT_SEARCH_REPOSITORY_PATH || "search/repositories";
 
 export const REPOSITORY_FULL_NAME = 'full_name';
 export const REPOSITORY_CONTENT_URL = 'contents_url';
